7077c4f86443d44018c5d06de681275a5c4e2127
zyf <2695274770@qq.com>
2024-10-25 08:51:27 +0800

    Keep calm and commit

--- ssy-ui-vite/vite.config.ts
+++ ssy-ui-vite/vite.config.ts
@@ -1,35 +1,42 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
 // import { presetUno, presetAttributify, presetIcons } from "unocss";
-import Unocss from "./config/unocss";
+import UnoCSS from 'unocss/vite'
 
 const rollupOptions = {
-  external: ["vue", "vue-router"],
+  external: ['vue', 'vue-router'],
   output: {
     globals: {
-      vue: "Vue",
+      vue: 'Vue',
     },
   },
-};
+}
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(),
-    Unocss(),],
-    build: {
-        rollupOptions,
-        minify: false,
-        cssCodeSplit: true,
-        lib: {
-          entry: "./src/entry.ts",
-          name: "SSYUI",
-          fileName: "ssy-ui",
-          formats: ["es", "umd", "iife"],
-        },
+  plugins: [vue(), vueJsx(), UnoCSS()],
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: './src/entry.ts',
+      name: 'SSYUI',
+      fileName: 'ssy-ui',
+      formats: ['es', 'umd', 'iife'],
+    },
+  },
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
   },
   resolve: {
     alias: {
-      vue: "vue/dist/vue.esm-bundler",
+      vue: 'vue/dist/vue.esm-bundler',
     },
   },
-});
+})
